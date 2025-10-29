b4c637d8c9dddd0b6002f553580585b0ca8ee87f
code-of-kai <97996644+code-of-kai@users.noreply.github.com>
2025-10-29 16:50:11 +0000

    Bump version to v1.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GitFoil.MixProject do
   def project do
     [
       app: :git_foil,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
